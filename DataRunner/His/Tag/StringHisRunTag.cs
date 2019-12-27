13a50881054924917f2cc2ed2106b5d4005c674a
Candy <cdy816@hotmail.com>
2019-12-27 18:51:07 +0800

    增加版权信息

--- DataRunner/His/Tag/StringHisRunTag.cs
+++ DataRunner/His/Tag/StringHisRunTag.cs
@@ -1,4 +1,12 @@
-﻿using Cdy.Tag;
+﻿//==============================================================
+//  Copyright (C) 2019  Inc. All rights reserved.
+//
+//==============================================================
+//  Create by 种道洋 at 2019/12/27 18:45:02.
+//  Version 1.0
+//  种道洋
+//==============================================================
+using Cdy.Tag;
 using System;
 using System.Collections.Generic;
 using System.Text;

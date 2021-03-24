e04150c19a11aa4740aa1beb525b7fa5d3c5016c
Candy <cdy816@hotmail.com>
2021-03-24 09:29:33 +0800

    修复人员编辑的时候出现的显示问题

--- Develop/DbManager.Desktop/View/Convert/IndexConverter.cs
+++ Develop/DbManager.Desktop/View/Convert/IndexConverter.cs
@@ -11,6 +11,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
+using System.Windows;
 using System.Windows.Data;
 
 namespace DBInStudio.Desktop
@@ -94,6 +95,38 @@ namespace DBInStudio.Desktop
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             return !(bool)value;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public class BoolInvertToVisiableConvert : IValueConverter
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value?Visibility.Visible:Visibility.Hidden;
         }
 
         /// <summary>
